6de16dbd572348008cea5f4072547262eb6a5bef
verygooder <107759587@qq.com>
2017-12-18 15:16:38 +0800

    add reddit.py in a raw way

---
+++ reddit.py
@@ -0,0 +1,35 @@
+import praw
+from pycolor import print_color
+
+
+class Red(praw.Reddit):
+    """docstring for Red"""
+
+    def __init__(self):
+        super(Red, self).__init__(client_id='GSaafCzmidYIBg', client_secret='STJUANi0ifHpbSQC122a6ks0qLk', username='verygooder', password='Ka840915', user_agent='luo\'s client')
+        if self.user.me() != 'verygooder':
+            print('login error!')
+            exit()
+        else:
+            print('login successful')
+
+    def get_info(self, subreddit_name):
+        sub = self.subreddit(subreddit_name)
+        hots = list(sub.hot(limit=30))
+        hots = sorted(hots, key=lambda x: x.score, reverse=False)
+        for i in hots:
+            print_color(i.title, fore='red')
+            items = [str(i.score), i.url]
+            string = '\t'.join(items)
+            print(string)
+            print('=' * 30)
+
+
+if __name__ == '__main__':
+    r = Red()
+    while 1:
+        sub = input('input the subreddit you need to watch:')
+        if sub == 'q':
+            exit()
+        else:
+            r.get_info(sub)
