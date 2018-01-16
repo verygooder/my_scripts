4b985811fbb218f60ba7f73e8a225b331b29510b
verygooder <107759587@qq.com>
2018-01-16 13:02:56 +0800

    add bilidynamic cookie read way

--- bili/bilidynamic.py
+++ bili/bilidynamic.py
@@ -11,10 +11,12 @@ def get_info():
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, br',
         'Connection': 'keep-alive',
-        'Cookie': 'l=v; LIVE_BUVID=c40ae155cb78121f2e07ec76605e0f1f; buvid3=A8F8CB4D-26D6-4A58-941E-13A233F3F52826540infoc; sid=63fc3x9r; fts=1510722266; LIVE_BUVID__ckMd5=00c4d10af2f0973a; rpdid=olwimixlkidosoiwlqoww; LIVE_PLAYER_TYPE=2; finger=dbd0cac5; DedeUserID=766568; DedeUserID__ckMd5=16c20f677710b2d7; SESSDATA=ce23596f%2C1515915147%2C9dfd6100; bili_jct=7cd98746cf284b40d5cfe82aec17f4e3; _dfcaptcha=0b5ed5479256d03bfe03222f671cb0fa; _cnt_pm=0; _cnt_notify=39',
         'Host': 'api.vc.bilibili.com',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:56.0) Gecko/20100101 Firefox/56.0'
     }
+    with open('./dyn_cookie', 'r') as f:
+        cookie = f.read().strip()
+    headers['Cookie'] = cookie
     url = 'https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/dynamic_new?uid=766568&type=268435455'
     r = requests.get(url, headers=headers)
     info = json.loads(r.text)
