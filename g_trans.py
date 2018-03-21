77acf882c301d0c4d53b9d7d7938f1aceed8a899
verygooder <107759587@qq.com>
2018-03-21 17:14:11 +0800

    google translate can switch between Chinese and English

--- g_trans.py
+++ g_trans.py
@@ -2,13 +2,18 @@ from googletrans import Translator
 from sys import argv
 
 
-def trans(string):
+def trans(string, dest_language):
     trans = Translator(service_urls=['translate.google.cn'])
     ori = string
-    result = trans.translate(ori, dest='zh-CN').text
+    result = trans.translate(ori, dest=dest_language).text
     return (ori, result)
 
 
-string = argv[1]
-trans_result = trans(string)[1]
+dest_dic = {
+    'c': 'zh-CN',
+    'e': 'en'
+}
+dest_language = dest_dic[argv[1]]
+string = argv[2]
+trans_result = trans(string, dest_language)[1]
 print(trans_result)
