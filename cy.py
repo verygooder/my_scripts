af4fc6a9ff35a5ff1b3003f2122458a6142a533e
罗宏敏 <luohongmin@luohongmindeMacBook-Pro.local>
2020-07-27 15:27:09 +0800

    caiyun add chinese support

--- cy.py
+++ cy.py
@@ -12,10 +12,10 @@ class Translator(object):
             'x-authorization': "token " + self.token
         }
 
-    def trans_sentences(self, source_sentences):
+    def trans_sentences(self, source_sentences, lang_argv):
         payload = {
             "source": source_sentences,
-            "trans_type": "en2zh",
+            "trans_type": {"-c": "en2zh", "-e": "zh2en"}[lang_argv],
             "request_id": "demo"
         }
         r = requests.request("POST", self.url, data=json.dumps(payload), headers=self.headers)
@@ -25,19 +25,19 @@ class Translator(object):
         else:
             return ['network error']
 
-    def trans_paragraph(self, paragraph):
+    def trans_paragraph(self, paragraph, lang_argv):
         sentences = paragraph.split('.')
         sentences = [i for i in sentences if i != '']
         sentences = [i + '.' for i in sentences]
-        trans_result = self.trans_sentences(sentences)
+        trans_result = self.trans_sentences(sentences, lang_argv)
         result = ''.join(trans_result)
         return result
 
 
 def main():
-    data = argv[1]
+    data = argv[2]
     tr = Translator()
-    result = tr.trans_paragraph(data)
+    result = tr.trans_paragraph(data, argv[1])
     print(result)
 
 
